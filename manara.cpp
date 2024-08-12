b6d947a87d263dd290cd447a9fc7108e50784e05
Manarabenali <sci_223445@omu.edu.ly>
2024-08-13 08:05:25 +1300

    Upload file cpp for testing

---
+++ manara.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    int studentID;
+    cout<<"enter your name:"<<endl;
+    getline(cin,name);
+    cout<<" enter youre ID"<<endl;
+    cin>>studentID;
+    cout<<"name: "<<name<<endl<<"studentID: "<<studentID;
+    
+
+    return 0;
+} 
